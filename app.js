5b68f90cf545d8fc43738df31ec48cedc1ba406c
inanna2930 <brittney_ross@outlook.com>
2024-04-18 16:15:09 -0400

    Created jss, cs, html files and image folder

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContent-Loaded', function() {
+    const popupContainer = document.getElementById('popup-container');
+    
+});
+
+function closePopup() {
+    const popupContainer = document.getElementById('popup-container');
+    popupcontainer.style.display = 'none';
+}
+
+
